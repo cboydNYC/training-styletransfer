298c57280577b5fb472bb5cdc71b79d1ed2cdead
Christopher Boyd <cboydNYC@users.noreply.github.com>
2019-06-23 16:59:11 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 export PYTHONUNBUFFERED=0
-python style.py --style images/aldunate.jpg \
+python style.py --style images/nakedpussy.jpg \
   --checkpoint-dir checkpoints/ \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
   --train-path /datasets/coco/coco_train2014 \
   --model-dir /artifacts \
-  --test images/violetaparra.jpg \
+  --test images/clare.jpg \
   --test-dir tests/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
